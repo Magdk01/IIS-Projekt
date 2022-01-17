b12f90ed2f8c48bb0dbec0cfe405ca93785d3929
Magdk01 <Magnusvinjebo@hotmail.com>
2022-01-17 15:48:24 +0100

    Custom data sæt generator og main der bruger denne

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import cv2
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -12,21 +13,49 @@ from skimage.io import imread
 from skimage.color import rgb2lab, lab2rgb
 from torch.nn import BCEWithLogitsLoss
 import os, time
-
+from torch.utils.data import Dataset
 
 transform = transforms.Compose(
-        [
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
+    [
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
 
-        ])
+    ])
 
-# trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-#                                         download=False, transform=transform)
+# trainset = torchvision.datasets.Places365(root='./data', split='val', small=True, download=False,
+#                                           transform=transform)
 
-trainset = torchvision.datasets.Places365(root='./data', split='val', small=True, download=False,
-                                              transform=transform)
+image_path = './data/val_256_new'
+train_image_paths = []
+for pictures in os.listdir(image_path):
+    train_image_paths.append(os.path.join(image_path, pictures))
 
+
+class CustomDataset123(Dataset):
+    def __init__(self, image_paths, transform=False):
+        self.image_paths = image_paths
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        image_filepath = self.image_paths[idx]
+        image = Image.open(image_filepath)
+        image = image.convert('RGB')
+
+        label = 1
+        if self.transform is not None:
+            image = self.transform(image)
+
+        return image, label
+
+
+trainset = CustomDataset123(image_paths=train_image_paths, transform=transform)
+
+batch_size = 4
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
 
 if __name__ == '__main__':
 
@@ -34,27 +63,6 @@ if __name__ == '__main__':
     print(f"Using {device} device")
 
     start_time = time.time()
-
-    Size = 256
-
-
-    batch_size = 5
-
-
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                              shuffle=True, num_workers=2)
-    #
-    # testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-    #                                        download=False, transform=transform)
-    # testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-    #                                          shuffle=False, num_workers=2)
-    #
-    # classes = ('plane', 'car', 'bird', 'cat',
-    #            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-    #
-    #
-    #
-    #
 
     dataiter = (iter(trainloader))
     images, labels = dataiter.next()
@@ -68,20 +76,6 @@ if __name__ == '__main__':
     grayscale_copy_of_lab = copy_of_lab[:, 0]
     grayscale_copy_of_lab = torch.from_numpy(grayscale_copy_of_lab.astype('float32'))
     grayscale_copy_of_lab = torch.unsqueeze(grayscale_copy_of_lab, 1)
-
-    fig = plt.figure(figsize=(10, 7))
-
-    for index in range(batch_size):
-        fig.add_subplot(3, batch_size, index + 1)
-        # plt.title(f'Original{index + 1}')
-        plt.axis('off')
-        plt.imshow(original_set_numpy[:, :, index])
-
-    for index in range(batch_size):
-        fig.add_subplot(3, batch_size, batch_size + index + 1)
-        # plt.title(f'GRSC{index+1}')
-        plt.axis('off')
-        plt.imshow(original_set_numpy[:, :, index, 0], cmap='gray')
 
 
     class Generator(nn.Module):
@@ -124,38 +118,40 @@ if __name__ == '__main__':
     gen = Generator().to(device)
 
     # criterion = nn.L1Loss()
-    criterion = nn.MSELoss()
     # optimizer = torch.optim.SGD(gen.parameters(), lr=learning_rate, momentum=0.5)
-    optimizer = torch.optim.Adam(gen.parameters(), lr=learning_rate)
 
-    epochs = 20
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(gen.parameters(), lr=learning_rate)
+    epochs = 1
     total_loss = 0
+    epoch_loss_list = []
     for epoch in range(epochs):
+        epoch_loss = 0
 
         for i, data in enumerate(trainloader):
 
-            inp_img, labels = data
+            inp_img_inside, labels = data
 
-            original_set_tensor = inp_img
-            original_set_numpy = original_set_tensor.numpy()
-            original_set_numpy = np.transpose(original_set_numpy, (2, 3, 0, 1))
-            original_set_lab = rgb2lab(original_set_numpy)
-            new_set_lab = np.transpose(original_set_lab, (2, 3, 0, 1))
-            lab_grayscale = np.copy(new_set_lab)
-            lab_grayscale = lab_grayscale[:, 0]
-            lab_AB = np.copy(new_set_lab)
-            lab_AB = lab_AB[:, [1, 2]]
+            original_set_tensor_inside = inp_img_inside
+            original_set_numpy_inside = original_set_tensor_inside.numpy()
+            original_set_numpy_inside = np.transpose(original_set_numpy_inside, (2, 3, 0, 1))
+            original_set_lab_inside = rgb2lab(original_set_numpy_inside)
+            new_set_lab_inside = np.transpose(original_set_lab_inside, (2, 3, 0, 1))
+            lab_grayscale_inside = np.copy(new_set_lab_inside)
+            lab_grayscale_inside = lab_grayscale_inside[:, 0]
+            lab_AB_inside = np.copy(new_set_lab_inside)
+            lab_AB_inside = lab_AB_inside[:, [1, 2]]
 
-            lab_grayscale = torch.from_numpy(lab_grayscale.astype('float32'))
-            lab_grayscale = torch.unsqueeze(lab_grayscale, 1)
+            lab_grayscale_inside = torch.from_numpy(lab_grayscale_inside.astype('float32'))
+            lab_grayscale_inside = torch.unsqueeze(lab_grayscale_inside, 1)
 
-            lab_target = torch.from_numpy(lab_AB.astype('float32'))
+            lab_target_inside = torch.from_numpy(lab_AB_inside.astype('float32'))
 
             optimizer.zero_grad()
 
-            output = gen(lab_grayscale.to(device))
+            output = gen(lab_grayscale_inside.to(device))
 
-            loss = criterion(output, lab_target.to(device))
+            loss = criterion(output, lab_target_inside.to(device))
 
             loss.backward()
 
@@ -163,11 +159,20 @@ if __name__ == '__main__':
 
             total_loss += loss.item()
 
-            if i % 500 == 0:
+            epoch_loss += loss.item()
+
+            if i % 250 == 0:
                 print(f'Epoch = {epoch}, I = {i},  Loss: {total_loss / 500}, Time: {time.time() - start_time}')
                 total_loss = 0
+        epoch_loss_list.append(epoch_loss)
+        plt.plot(epoch_loss_list)
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
 
-    PATH = './Main_Gen1.1_Model_20Epochs_places365_valset_JB.pth'
+        plt.show()
+
+
+    PATH = './Main_Gen1.1_Model_1Epoch_hackeddataset_MSVN.pth'
     torch.save(gen.state_dict(), PATH)
 
     GeneratedAB_img = gen(grayscale_copy_of_lab.to(device))
@@ -177,9 +182,23 @@ if __name__ == '__main__':
 
     merged_img = np.concatenate((grayscale_copy_of_lab, GeneratedAB_img), axis=1)
 
+
+
+
+    fig = plt.figure(figsize=(10, 7))
+
+    for index in range(batch_size):
+        fig.add_subplot(3, batch_size, index + 1)
+        plt.axis('off')
+        plt.imshow(original_set_numpy[:, :, index])
+
+    for index in range(batch_size):
+        fig.add_subplot(3, batch_size, batch_size + index + 1)
+        plt.axis('off')
+        plt.imshow(original_set_numpy[:, :, index, 0], cmap='gray')
+
     for index in range(batch_size):
         fig.add_subplot(3, batch_size, batch_size * 2 + index + 1)
-        # plt.title(f'GRSC{index+1}')
         plt.axis('off')
         img_slice = merged_img[index]
         print_img = np.transpose(img_slice, (1, 2, 0))
